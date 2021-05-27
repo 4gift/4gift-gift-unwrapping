761b05154ba426f15dc30720c7b741b51852f39d
misterwolf <dario.gas@gmail.com>
2021-05-27 18:10:56 +0200

    update version - fix gift

--- unwrapping-new-0.8.2.js
+++ unwrapping-new-0.8.2.js
@@ -53,7 +53,7 @@ class GiftUnwrapping {
         var sourceBrandizedPath = sourcePath + brandName;
 
         if (giftUrl === "") {
-            giftUrl = sourceBrandizedPath +  "/desktop/gift@2x.png"
+            giftUrl = sourceBrandizedAssetPath +  "/desktop/gift@2x.png"
         }
 
         const head  = document.getElementsByTagName("head")[0];
